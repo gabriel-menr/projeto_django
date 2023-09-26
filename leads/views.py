9952bb51f89b9de21205f855b37530442b8805d3
Gabriel Menino <gabrielm@localhost.localdomain>
2023-09-26 12:09:04 -0300

    font aprimored

--- leads/views.py
+++ leads/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
+from django.views.generic import TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Lead, Agent
 from .forms import LeadForm,LeadModelForm
- 
+
+class LandingPageView(TemplateView):
+    template_name = "landing.html"
+
+class LeadListView(ListView):
+    template_name = "lead_list.html"
+    queryset = Lead.objects.all()
+    context_object_name = "leads"
+
+class LeadDetailView(DetailView):
+    template_name = "leads/lead_detail.html"
+    model = Lead
+    context_object_name = "lead"
+
+class LeadCreateView(CreateView):
+    template_name = "leads/lead_create.html"
+    form_class = LeadModelForm
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+class LeadUpdateView(UpdateView):
+    template_name = "leads/lead_update.html"
+    queryset = Lead.objects.all()
+    form_class = LeadModelForm
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
+class LeadDeleteView(DeleteView):
+    template_name = "leads/lead_delete.html"
+    queryset = Lead.objects.all()
+    def get_success_url(self):
+        return reverse("leads:lead-list")
+
 def landing_page(request):
     return render(request,"landing.html")
 
@@ -50,25 +83,25 @@ def lead_delete(request, pk):
     lead = Lead.objects.get(id=pk)
     lead.delete()
     return redirect("/leads")
-# def lead_update(request,pk):
-#     lead = Lead.objects.get(id=pk)
-#     form = LeadForm()
-#     if request.method == "POST":
-#         form = LeadModelForm(request.POST)
-#         if form.is_valid():
-#             first_name = form.cleaned_data['first_name']
-#             last_name = form.cleaned_data['last_name']
-#             age = form.cleaned_data['age']
-#             agent= Agent.objects.first()   
-#             lead.first_name = first_name
-#             lead.last_name = last_name
-#             lead.age = age
-#             lead.save()
-#             return redirect("/leads")
-    # context = {
-    #     "form" : form,
-    #     "lead" : lead}
-#     return render(request,"leads/lead_update.html", context)
+def lead_update(request,pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadForm()
+    if request.method == "POST":
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent= Agent.objects.first()   
+            lead.first_name = first_name
+            lead.last_name = last_name
+            lead.age = age
+            lead.save()
+            return redirect("/leads")
+    context = {
+        "form" : form,
+        "lead" : lead}
+    return render(request,"leads/lead_update.html", context)
 
 
 
